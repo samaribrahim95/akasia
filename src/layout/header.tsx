2ed96e84524c014d222c686640558732b2c1fcbc
Samar Ibrahim <samaribrahim@Samars-iMac.local>
2025-01-11 22:04:12 +0300

    config dark mode

---
+++ src/layout/header.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useEffect } from "react";
+
+
+const Header = () => {
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  }
+
+  return (
+    <button onClick={toggleTheme}>toggle dark mode</button>
+  )
+
+}
+
+export default Header
