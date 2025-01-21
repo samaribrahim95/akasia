915bc4dbb5a4645b6e079ef01b990100d3575be0
Samar Ibrahim <samaribrahim@Samars-iMac.local>
2025-01-21 22:46:25 +0300

    deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  basePath: 'ttps://github.com/samaribrahim95/akasia'
+};
 
 export default withNextIntl(nextConfig);
