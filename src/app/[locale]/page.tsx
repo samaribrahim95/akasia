5bcd7e25a2351795cfb7173ccd317ca4043fac29
Samar Ibrahim <samaribrahim@Samars-iMac.local>
2025-01-23 16:03:46 +0300

    brief

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,14 @@
 import Solutions from "@/components/solutions";
 import Header from "../../components/header";
 import ContactInfo from "@/components/contactInfo";
+import Brief from "@/components/brief";
 
 export default function Home() {
   return (
     <>
     <Header />
     <Solutions />
+    <Brief />
     <ContactInfo />
     </>
   );
