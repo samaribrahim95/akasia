2fbfa61829181c63a6fe4154e571dbbff00cdd3b
samaribrahim95 <samar.ibrahim1995@gmail.com>
2025-01-26 14:55:12 +0200

    fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -28,9 +28,9 @@ export async function generateMetadata({
 
 export default async function RootLayout({
   children,
-  params,
+  params: { locale },
 }: Readonly<Props>) {
-  const { locale } = await params;
+  // const { locale } = await params;
 
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as LocalesType)) {
