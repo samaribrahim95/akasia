2a4191063fd2dd955862b8e317b6a1fd083444f9
samaribrahim95 <samar.ibrahim1995@gmail.com>
2025-01-26 22:28:24 +0200

    mobile

---
+++ src/layout/mobileMenu.tsx
@@ -0,0 +1,76 @@
+const MobileMenu = ({ opened }: { opened: boolean }) => {
+  return (
+    <div className={`absolute ${opened ? 'left-0' : 'left-full'} top-0 w-full h-[100dvh] flex flex-col items-center p-3 transition-all duration-500 ease-in-out backdrop-blur-xl bg-black z-[49]`}>
+      <div className="pt-16 flex h-[100dvh] flex-col items-start justify-between w-full max-w-[1000px] z-[999]">
+        <div>
+          <ul>
+            <li id="f-about" className="mt-3 flex flex-col justify-center gap-2 items-start sm:justify-start sm:items-start">
+              <a href="/about/">
+                <h4 className="text-gray-900 dark:text-gray-50">About Akasia Capital</h4>
+              </a>
+            </li>
+            <li id="f-solutions" className="mt-3 flex flex-col justify-center gap-2 items-start sm:justify-start sm:items-start">
+              <a href="/investment/">
+                <h4 className="text-gray-900 dark:text-gray-50">How to Invest</h4>
+              </a>
+            </li>
+            <li id="f-ContactUs" className="mt-3 flex flex-col justify-center gap-2 items-start sm:justify-start sm:items-start">
+              <a href="/contactus/">
+                <h4 className="text-gray-900 dark:text-gray-50">Contact</h4>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="w-full flex flex-col gap-6">
+          <div id="f-contactus" className="mt-3">
+            <h4 className="text-gray-900 dark:text-gray-50">Contact</h4>
+            <ul>
+              <li>
+                <a href="tel:+966114792233">
+                  <span className="text-gray-900 dark:text-gray-50">Phone: +966114442233</span>
+                </a>
+              </li>
+              <li>
+                <a href="mailto:info@getakasia.com">
+                  <span className="text-gray-900 dark:text-gray-50">Email: info@getakasia.com</span>
+                </a>
+              </li>
+              <li>
+                <a href="/#">
+                  <span className="text-gray-900 dark:text-gray-50">Riyadh, Al Malqa, Saudi Arabia</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="w-full flex flex-row items-center justify-between dark:border-t-slate-500/50 border-t-gray-300/50 border-t pt-6 pb-6">
+            <a>
+              <button aria-label="Toggle dark mode" className="">
+                <div className="flex flex-row gap-2 justify-start items-center">
+                  <span className="dark:block hidden text-gray-900 dark:text-gray-50 font-bold">Light Colors</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <circle cx="12" cy="12" r="5"></circle>
+                    <line x1="12" y1="1" x2="12" y2="3"></line>
+                    <line x1="12" y1="21" x2="12" y2="23"></line>
+                    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                    <line x1="1" x2="3" y1="12" y2="12"></line>
+                    <line x1="21" x2="23" y1="12" y2="12"></line>
+                    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                  </svg>
+                </div>
+              </button>
+            </a>
+            <div className="flex flex-row items-center justify-centers gap-2">
+              <button className="font-rubik text-black dark:text-white">
+                <span className="font-semibold">العربية</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MobileMenu
--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import logoTxt from "../assets/images/akasia-txt-logo.svg";
 import logoTxtLight from "../assets/images/akasia-txt-logo-light.svg";
 import { useTranslations } from "next-intl";
 import { usePathname, useRouter } from "next/navigation";
+import MenuBarIcon from "@/components/dynamicIcons/menuBar";
+import CloseIcon from "@/components/dynamicIcons/close";
+import MobileMenu from "./mobileMenu";
 
 const Navbar = ({ locale }: { locale: string }) => {
   const t = useTranslations("NavBar");
   const pathname = usePathname();
   const router = useRouter();
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
 
   const menuItem = [
     {
@@ -33,89 +37,98 @@ const Navbar = ({ locale }: { locale: string }) => {
     router.push(`/${newLocale}/${path}`);
   };
 
-  return (
-    <nav className="fixed w-full z-50 top-0 backdrop-blur-md border-b-blue-900/5 dark:border-b-gray-100/5 border-b shadow-lg">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <div id="nav-logo">
-          <div className="hidden dark:flex">
-            <Link href="/">
-              <Image
-                alt="logo - akasia"
-                loading="lazy"
-                width="70"
-                height="50"
-                className="object-cover object-center"
-                src={logoTxtLight}
-              />
-            </Link>
-          </div>
-          <div className="flex dark:hidden">
-            <Link href="/">
-              <Image
-                alt="logo - akasia"
-                width="70"
-                height="50"
-                className=""
-                src={logoTxt}
-              />
-            </Link>
-          </div>
-        </div>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:border-gray-700">
-            {menuItem.map((items, index) => (
-              <li key={index}>
-                <Link
-                  href={items.to}
-                  className="font-medium text-black dark:text-slate-100"
-                //   aria-current="page"
-                >
-                  {items.title}
-                </Link>
-              </li>
-            ))}
+  useEffect(() => {
+    if (openMenu) document.body.style.overflow = "hidden"
+    else document.body.style.overflow = "auto"
+  }, [openMenu])
 
-            <li>
-              <button
-                onClick={() => toggleLang(locale == "ar" ? "en" : "ar")}
-                className="font-medium text-black dark:text-slate-100"
-              >
-                {locale == "ar" ? "EN" : "ع"}
-              </button>
-            </li>
-            <li>
-              <button className="bg-gradient-to-bl from-slate-500 to-indigo-300 hover:bg-blue-200 text-white py-1 px-3 inline-flex items-center rounded-full">
-                <span>{t("downloadApp")}</span>
-              </button>
-            </li>
-          </ul>
+  return (
+    <div className="overflow-hidden w-full">
+      <nav className="fixed w-full z-[99] top-0 backdrop-blur-md border-b-blue-900/5 dark:border-b-gray-100/5 border-b shadow-lg">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div id="nav-logo">
+            <div className="hidden dark:flex">
+              <Link href="/">
+                <Image
+                  alt="logo - akasia"
+                  loading="lazy"
+                  width="70"
+                  height="50"
+                  className="object-cover object-center"
+                  src={logoTxtLight}
+                />
+              </Link>
+            </div>
+            <div className="flex dark:hidden">
+              <Link href="/">
+                <Image
+                  alt="logo - akasia"
+                  width="70"
+                  height="50"
+                  className=""
+                  src={logoTxt}
+                />
+              </Link>
+            </div>
+          </div>
+          <div className="md:hidden flex items-end gap-4">
+            <button
+              onClick={() => toggleLang(locale == "ar" ? "en" : "ar")}
+              className="font-medium text-black dark:text-slate-100"
+            >
+              {locale == "ar" ? "EN" : "ع"}
+            </button>
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className="w-10 h-5 text-gray-500 focus:outline-none dark:text-gray-400 text-center"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+              onClick={() => setOpenMenu(!openMenu)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <span className="mx-auto inline-block transition-all">
+                {
+                  openMenu && <CloseIcon />
+                  || <MenuBarIcon />
+                }
+              </span>
+            </button>
+          </div>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:border-gray-700">
+              {menuItem.map((items, index) => (
+                <li key={index}>
+                  <Link
+                    href={items.to}
+                    className="font-medium text-black dark:text-slate-100"
+                  //   aria-current="page"
+                  >
+                    {items.title}
+                  </Link>
+                </li>
+              ))}
+
+              <li>
+                <button
+                  onClick={() => toggleLang(locale == "ar" ? "en" : "ar")}
+                  className="font-medium text-black dark:text-slate-100"
+                >
+                  {locale == "ar" ? "EN" : "ع"}
+                </button>
+              </li>
+              <li>
+                <button className="bg-gradient-to-bl from-slate-500 to-indigo-300 hover:bg-blue-200 text-white py-1 px-3 inline-flex items-center rounded-full">
+                  <span>{t("downloadApp")}</span>
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <MobileMenu opened={openMenu} />
+
+    </div>
   );
 };
 
